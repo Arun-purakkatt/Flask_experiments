83ea6ca3dcf4c51307324353649b71b1811af2d3
Arun-purakkatt <55660573+Arun-purakkatt@users.noreply.github.com>
2020-06-26 14:06:30 +0530

    Add files via upload

---
+++ demo2.py
@@ -0,0 +1,23 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html", content="testing", x=4)
+
+
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}!"
+
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("user",
+                            name="Admin!"))  # Now we when we go to /admin we will redirect to user with the argument "Admin!"
+
+
+if __name__ == "__main__":
+    app.run()
